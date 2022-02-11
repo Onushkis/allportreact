63a7aedac0d2f7b7290b61df9f23f1edf7f529ce
Ona <onushkis@gmail.com>
2022-02-11 16:24:23 +0100

    add changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+
+     <h1 className="text-green-400 text-center text-mid border-8 border-[#243c5a]">miauona</h1>
+      </div>
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+    theme: {
+      fontSize: {
+        'small': '0.813rem',
+        'main':'1.875rem',
+        'mid': '1.125rem',
+      },
+        
+    extend: {
+      colors: {
+        'regal-violetbtn': '#A594EB',
+      }
+    },
+  },
+  plugins: [],
+}
